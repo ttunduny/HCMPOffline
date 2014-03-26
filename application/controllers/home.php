718b16099f2ef9a793495ec3850e9c44666863f4
Mureithi <kelvinmwas@gmail.com>
2014-03-26 11:01:46 +0300

    new merge

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -11,48 +11,47 @@ class Home extends MY_Controller {
 		$this -> load -> library(array('hcmp_functions', 'form_validation'));
 	}
 
-	/
-
 	public function index() {
 
-		$identifier = $this -> session -> userdata('user_indicator');
+		/**
+		 * [$identifier description]
+		 * saves value of user type
+		 * @var [varchar]
+		 */
+			$identifier = $this -> session -> userdata('user_indicator');
 
-		if ($identifier == 'moh') {
-			$data['content_view'] = "";
-			$view = 'shared_files/template/dashboard_template_v';
-		}
-		if ($identifier == 'facility_admin') {
-			$data['content_view'] = "";
-			$view = 'shared_files/template/template';
-		}
-		if ($identifier == 'district') {
-			$data['content_view'] = "";
-			$view = 'shared_files/template/dashboard_template_v';
-		}
-
-		if ($identifier == 'moh_user') {
-			$data['content_view'] = "";
-		}
-
-		if ($identifier == 'facility') {
-			$data['content_view'] = "facility/facility_home_v";
-			$view = 'shared_files/template/template';
-		}
-
-		if ($identifier == 'district_tech') {
-			$data['content_view'] = "";
-		}
-
-		if ($identifier == 'rtk_manager') {
-			$data['content_view'] = "";
-		}
-
-		if ($identifier == 'super_admin') {
-			$data['content_view'] = "";
-		}
-
-		if ($identifier == 'allocation_committee') {
-			$data['content_view'] = "";
+		switch ($identifier) {
+			case moh :
+				$data['content_view'] = "";
+				$view = 'shared_files/template/dashboard_template_v';
+				break;
+			case facility_admin :
+				$data['content_view'] = "";
+				$view = 'shared_files/template/template';
+				break;
+			case district :
+				$data['content_view'] = "";
+				$view = 'shared_files/template/dashboard_template_v';
+				break;
+			case moh_user :
+				$data['content_view'] = "";
+				break;
+			case facility :
+				$data['content_view'] = "facility/facility_home_v";
+				$view = 'shared_files/template/template';
+				break;
+			case district_tech :
+				$data['content_view'] = "";
+				break;
+			case rtk_manager :
+				$data['content_view'] = "";
+				break;
+			case super_admin :
+				$data['content_view'] = "";
+				break;
+			case allocation_committee :
+				$data['content_view'] = "";
+				break;
 		}
 
 		$data['title'] = "System Home";
