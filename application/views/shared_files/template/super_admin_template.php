388ca702c349af8d2c78f291bf448f074aca2d65
Mureithi <kelvinmwas@gmail.com>
2014-06-20 13:09:32 +0300

    Regress

--- application/views/shared_files/template/super_admin_template.php
+++ application/views/shared_files/template/super_admin_template.php
@@ -11,7 +11,7 @@ box-shadow:         3px 0px 5px 0px rgba(51, 50, 50, 0.59);
 	}	
 </style>
 
-
+<div class="container">
 
 <div class="row" style="padding-top: 1%" >
 	
@@ -50,30 +50,95 @@ box-shadow:         3px 0px 5px 0px rgba(51, 50, 50, 0.59);
 					<div class="color_b stat_item">
 						<span class="glyphicon glyphicon-pushpin"></span>
                   
-                    xyz      
+                    notifications     
                    </div>
 	</div>
 	<div class="col-md-2">
 					<div class="color_a stat_item">
 					<span class="glyphicon glyphicon-dashboard"></span>
                   
-                    qwe      
+                    notifications      
                    </div>
 	</div>
 	
 </div>
 
-<div class="row" style="margin-top: 1%" >
+</div>
+
+<div class="container-fluid" style="border: 0px solid #036;">
+
+<div class="row" style="margin-top: 1%;border: 0px solid #036;" >
 
 
-<div class="col-md-7" style="height:300px;border: 0px solid #036;" id="container">
-
+<div class="col-md-6"  id="">
+<div class="row" style="margin-left: 0px;"> 
+	<div class="col-md-12" style="height:350px;border: 0px solid #036;">
+	
+	</div>
+	
+	</div>
+	
+	<div class="row" style="margin-left: 0px;"> 
+	<div class="col-md-12" style="border: 0px solid #036;">
+	
+	<div class="panel panel-default">
+  <!-- Default panel contents -->
+  	<div class="panel-heading">Panel heading</div>
+<div class="panel-body">
+    <div id="container">
+    	
+    </div>
+  </div>
+  
+	</div>
+	
+	</div>
+	
+	</div>
 
 
 </div>
-<div class="col-md-5" style="height:300px;border: 0px solid #036;">
+<div class="col-md-6" >
+	<div class="row" style="margin-left: 0px;"> 
+	<div class="col-md-12" style="border: 0px solid #036;">
+		
+		<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Panel heading</div>
+
+  <div class="panel-body">
+    <div id="container2">
+    	
+    </div>
+  </div>
+  
+	</div>
+	
+	</div>
+	
+	</div>
+	
+	<div class="row" style="margin-left: 0px;"> 
+	<div class="col-md-12" style="border: 0px solid #036;">
+		
+		<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Panel heading</div>
+
+  <div class="panel-body">
+    <div id="container3">
+    	
+    </div
+  </div>
+  
+	</div>
+	
+	</div>
+	
+	</div>
 
 
+</div>
 
 </div>
 
@@ -81,6 +146,115 @@ box-shadow:         3px 0px 5px 0px rgba(51, 50, 50, 0.59);
 
 <script>
 $(document).ready(function () {
+	
+	$(function () {
+        $('#container2').highcharts({
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Monthly Average Temperature'
+            },
+            subtitle: {
+                text: 'Source: WorldClimate.com'
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+                title: {
+                    text: 'Temperature (°C)'
+                }
+            },
+            plotOptions: {
+                line: {
+                    dataLabels: {
+                        enabled: true
+                    },
+                    enableMouseTracking: false
+                }
+            },
+            series: [{
+                name: 'Tokyo',
+                data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            }, {
+                name: 'London',
+                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            }]
+        });
+    });
+    
+
+	
+	
+	$(function () {
+        $('#container3').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Stacked column chart'
+            },
+            xAxis: {
+                categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Total fruit consumption'
+                },
+                stackLabels: {
+                    enabled: true,
+                    style: {
+                        fontWeight: 'bold',
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                    }
+                }
+            },
+            legend: {
+                align: 'right',
+                x: -70,
+                verticalAlign: 'top',
+                y: 20,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                borderColor: '#CCC',
+                borderWidth: 1,
+                shadow: false
+            },
+            tooltip: {
+                formatter: function() {
+                    return '<b>'+ this.x +'</b><br/>'+
+                        this.series.name +': '+ this.y +'<br/>'+
+                        'Total: '+ this.point.stackTotal;
+                }
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'normal',
+                    dataLabels: {
+                        enabled: true,
+                        color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                        style: {
+                            textShadow: '0 0 3px black, 0 0 3px black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                name: 'John',
+                data: [5, 3, 4, 7, 2]
+            }, {
+                name: 'Jane',
+                data: [2, 2, 3, 2, 1]
+            }, {
+                name: 'Joe',
+                data: [3, 4, 4, 2, 5]
+            }]
+        });
+    });
+    
+
 	
 	$(function () {
     var chart;
