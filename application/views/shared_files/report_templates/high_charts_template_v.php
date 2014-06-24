15293206a0a6a9973124510ea18029ecf0f8730e
karsanrichard <karsanrichard@gmail.com>
2014-06-24 21:38:22 +0300

    after format

--- application/views/shared_files/report_templates/high_charts_template_v.php
+++
@@ -1,7 +0,0 @@
-<script>
-   $(document).ready(function() {
-       <?php echo $high_graph; ?>
-
-    });
-</script>
-<div style="/*border: 1px solid #036;*/ ;" id="dem_graph_"></div>
--- application/views/shared_files/template/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="footer">
-	Government of Kenya &copy; <?php echo date('Y'); ?>. All Rights Reserved
-	
-</div>
