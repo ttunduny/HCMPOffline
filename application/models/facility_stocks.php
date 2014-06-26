af8fa0361b597b1dfa060fbf2ae5d8b06e8b4dd2
kariukijackson <kariukijackson@gmail.com>
2014-06-26 12:56:44 +0300

    blank

--- application/models/facility_stocks.php
+++ application/models/facility_stocks.php
@@ -217,7 +217,6 @@ $year=null,$month=null,$option=null,$data_for=null)
 	 (isset($district_id) && !isset($facility_code) ?  " GROUP BY f.facility_code having total>0": 
 	 ($graph_type=='table_data')&& ($commodity_id>0) ?" GROUP BY d.id, f.facility_code having total>0 order by di.district asc, f.facility_name asc" :
 	 " GROUP BY d.id having total > 0") ;
-
 	
 	$inserttransaction = Doctrine_Manager::getInstance()->getCurrentConnection()
     ->fetchAll("SELECT  $selection_for_a_month $computation
