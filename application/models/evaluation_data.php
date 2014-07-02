c3649e77d2c1a859ae5e3cd62211b87cc0dfd99a
mesAdima <mesadima@gmail.com>
2014-07-02 11:53:41 +0300

    making the data dynamic-evaluation

--- application/models/evaluation_data.php
+++ application/models/evaluation_data.php
@@ -16,34 +16,41 @@
 	}
 
 
-		public static function get_personel_trained($county_id){
+		public static function get_personel_trained($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query2 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select sum(f_e.fhead_no) as fhead, sum(f_e.fdep_no) as fdep, sum(f_e.nurse_no) as nurse, sum(f_e.sman_no) as sman, sum(f_e.ptech_no) as ptech
 from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id");
+and f.district=d.id and d.county=$county_id
+$where
+");
 		
 		
 		return $query2;
 		
 	}
 
-		public static function get_training_satisfaction($county_id){
+		public static function get_training_satisfaction($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query3 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("select count(f_e.satisfaction_lvl) as level, f_e.satisfaction_lvl
 from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id group by f_e.satisfaction_lvl
+$where
 ");	
 		return $query3;
 		
 	}
-		public static function get_training_resource($county_id){
+		public static function get_training_resource($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;	
 		$query_1 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select count(f_e.comp_avail) as comp
 from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f_e.comp_avail=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
  $query_2 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -52,6 +59,7 @@ from facilities f, facility_evaluation f_e, districts d
 where f.facility_code=f_e.facility_code 
 and f_e.modem_avail=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
  $query_3 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -60,6 +68,7 @@ from facilities f, facility_evaluation f_e, districts d
 where f.facility_code=f_e.facility_code 
 and f_e.bundles_avail=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
  $query_4 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -68,6 +77,7 @@ from facilities f, facility_evaluation f_e, districts d
 where f.facility_code=f_e.facility_code 
 and f_e.manuals_avail=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
   $query_5 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -75,6 +85,7 @@ select COUNT( f_e.id ) AS total
 from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
 		return array(0=>array('total'=>$query_5[0]['total'],'comp'=>$query_1[0]['comp'],'modem'=>$query_2[0]['modem'],'bundles'=>$query_3[0]['bundles'],'manual'=>$query_4[0]['manual']));
@@ -82,150 +93,188 @@ AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
 	}
 
 
-		public static function get_sheduled_training($county_id){
+		public static function get_sheduled_training($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query4 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, districts d ,facility_evaluation f_e
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`agreed_time`) as actual, agreed_time from facilities f, districts d ,facility_evaluation f_e 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `agreed_time`
+and f.district=d.id and d.county=$county_id 
+$where
+group by `agreed_time`
 ");	
 		return $query4;	
 	}
 
-		public static function get_feedback_training($county_id){
+		public static function get_feedback_training($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query5 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`feedback`) as actual, feedback from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `feedback`
+and f.district=d.id and d.county=$county_id 
+$where
+group by `feedback`
 ");	
 		return $query5;
 	}
 
-		public static function get_pharm_supervision($county_id){
+		public static function get_pharm_supervision($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query6 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`pharm_supervision`) as actual, pharm_supervision from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `pharm_supervision`
+and f.district=d.id and d.county=$county_id
+$where
+group by `pharm_supervision`
 ");	
 		return $query6;
 		
 	}
-		public static function get_coord_supervision($county_id){
+		public static function get_coord_supervision($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query7 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`coord_supervision`) as actual, coord_supervision from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `coord_supervision`
+and f.district=d.id and d.county=$county_id
+$where
+group by `coord_supervision`
 ");	
 		return $query7;
 	}
 
-		public static function get_req_id($county_id){
+		public static function get_req_id($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query8 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`req_id`) as actual, req_id from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `req_id`
+and f.district=d.id and d.county=$county_id
+$where
+group by `req_id`
 ");	
 		return $query8;
 	}
 
 
-	public static function get_req_addr($county_id){
+	public static function get_req_addr($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query9 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`req_addr`) as actual, req_addr from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `req_addr`
+and f.district=d.id and d.county=$county_id
+$where
+group by `req_addr`
 ");	
 		return $query9;
 	}
 
 
-	public static function get_train_useful($county_id){
+	public static function get_train_useful($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query10 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`train_useful`) as actual, train_useful from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `train_useful`
+and f.district=d.id and d.county=$county_id
+$where
+group by `train_useful`
 ");	
 		return $query11;
 		
 	}
-				public static function get_use_freq($county_id){
+				public static function get_use_freq($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query12 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`use_freq`) as level, use_freq from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `use_freq`
+and f.district=d.id and d.county=$county_id
+$where
+group by `use_freq`
 ");	
 		return $query12;
 		
 	}
 
-	public static function get_improvement($county_id){
+	public static function get_improvement($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query13 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`improvement`) as actual, improvement from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `improvement`
+and f.district=d.id and d.county=$county_id
+$where
+group by `improvement`
 ");	
 		return $query13;
 		
 	}
-						public static function get_ease_of_use($county_id){
+						public static function get_ease_of_use($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;					
 		$query14 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`ease_of_use`) as actual, ease_of_use from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `ease_of_use`
+and f.district=d.id and d.county=$county_id
+$where
+group by `ease_of_use`
 ");	
 		return $query14;
 		
 	}
 
 
-	public static function get_meet_expect($county_id){
+	public static function get_meet_expect($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query15 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`meet_expect`) as actual, meet_expect from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `meet_expect`
+and f.district=d.id and d.county=$county_id
+$where
+group by `meet_expect`
 ");	
 		return $query15;
 		
 	}
-		public static function get_retrain($county_id){
+		public static function get_retrain($county_id=null,$district_id=null){
+		$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 		$query16 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select (select count(*) from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id ) as total, count(`retrain`) as actual, retrain from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
-and f.district=d.id and d.county=$county_id  group by `retrain`
+and f.district=d.id and d.county=$county_id
+$where
+group by `retrain`
 ");	
 		return $query16;
 		
 	}
-	public static function level_of_comfort($county_id){
+	public static function level_of_comfort($county_id=null,$district_id=null){
+			$where=(isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id " ;
 			$query_1 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 select  count(f_e.comf_issue) as comp
 from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f_e.comf_issue=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
  $query_2 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -234,6 +283,7 @@ from facilities f, facility_evaluation f_e, districts d
 where f.facility_code=f_e.facility_code 
 and f_e.comf_order=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
  $query_3 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -242,6 +292,7 @@ from facilities f, facility_evaluation f_e, districts d
 where f.facility_code=f_e.facility_code 
 and f_e.comf_update=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
  $query_4 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -250,6 +301,7 @@ from facilities f, facility_evaluation f_e, districts d
 where f.facility_code=f_e.facility_code 
 and f_e.comf_gen=1
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
   $query_5 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -257,6 +309,7 @@ select COUNT( f_e.id ) AS total
 from facilities f, facility_evaluation f_e, districts d 
 where f.facility_code=f_e.facility_code 
 and f.district=d.id and d.county=$county_id
+$where
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
  ");	
 		return array(0=>array('total'=>$query_5[0]['total'],'comp'=>$query_1[0]['comp'],'modem'=>$query_2[0]['modem'],'bundles'=>$query_3[0]['bundles'],'manual'=>$query_4[0]['manual']));
@@ -264,13 +317,15 @@ AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
 		
 	}
 
-	public static function get_district_coverage_data($county_id){
+	public static function get_district_coverage_data($county_id = null, $district_id = null){
+	 $where = (isset($county_id) && !isset($district_id)) ? " and d.county=$county_id " : " and d.id=$district_id ";
    	 $query_1 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
 SELECT COUNT( f.id ) AS total
 FROM facilities f, districts d
 WHERE f.district = d.id
 AND d.county ='$county_id'
 AND UNIX_TIMESTAMP( f.`date_of_activation` ) >0
+$where
  ");
  
    	 $query_2 = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAll("
@@ -279,6 +334,7 @@ FROM facilities f, districts d, facility_evaluation h_s
 WHERE f.district = d.id
 AND d.county ='$county_id'
 AND h_s.facility_code=f.facility_code
+$where
  ");
    return array("total_facilities"=>$query_1[0]['total'],'total_evaluation'=>$query_2[0]['total']);
    }
