af5c9710d5fb77c2b6910db083518fa47c2ea47e
RiscaJack <kariukijackson@gmail.com>
2014-07-03 10:15:41 +0300

    merge fix

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = '#cmP@2014';//#cmP@2014
 $db['default']['database'] = 'hcmp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
