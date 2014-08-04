acbaccd7ce93a0bf3adde8b40097926bddc73be5
Jackson <kariukijackson@gmail.com>
2014-08-04 18:41:44 +0300

    test

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -10,7 +10,7 @@ class User extends MY_Controller {
 
 	function __construct() {
 		parent::__construct();
-
+ 
 		$this -> load -> helper(array('form', 'url'));
 		$this -> load -> library(array('hcmp_functions', 'form_validation'));
 	}
