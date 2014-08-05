5e4d3cdb1bbc7cee4e99cf065c69d0f7012c9dad
Jackson <kariukijackson@gmail.com>
2014-08-05 13:19:31 +0300

    test

--- assets/scripts/hcmp_shared_functions.js
+++ assets/scripts/hcmp_shared_functions.js
@@ -147,6 +147,70 @@ function dialog_box(body_html_data,footer_html_data){
             $('#communication_dialog .modal-footer').html(footer_html_data);
             $('#communication_dialog .modal-body').html(body_html_data);
             $('#communication_dialog').modal('show');
+               //file uploader 
+    var reader;
+  var progress = document.querySelector('.percent');
+
+  function abortRead() {
+    reader.abort();
+  }
+
+  function errorHandler(evt) {
+    switch(evt.target.error.code) {
+      case evt.target.error.NOT_FOUND_ERR:
+        alert('File Not Found!');
+        break;
+      case evt.target.error.NOT_READABLE_ERR:
+        alert('File is not readable');
+        break;
+      case evt.target.error.ABORT_ERR:
+        break; // noop
+      default:
+        alert('An error occurred reading this file.');
+    };
+  }
+
+  function updateProgress(evt) {
+    // evt is an ProgressEvent.
+    if (evt.lengthComputable) {
+      var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+      // Increase the progress bar length.
+      if (percentLoaded < 100) {
+        progress.style.width = percentLoaded + '%';
+        progress.textContent = percentLoaded + '%';
+      }
+    }
+  }
+
+  function handleFileSelect(evt) {
+    // Reset progress indicator on new file selection.
+    progress.style.width = '0%';
+    progress.textContent = '0%';
+
+    reader = new FileReader();
+    reader.onerror = errorHandler;
+    reader.onprogress = updateProgress;
+    reader.onabort = function(e) {
+      alert('File read cancelled');
+    };
+    reader.onloadstart = function(e) {
+      document.getElementById('progress_bar').className = 'loading';
+    };
+    reader.onload = function(e) {
+      // Ensure that the progress bar displays 100% at the end.
+      progress.style.width = '100%';
+      progress.textContent = '100%';
+      setTimeout("document.getElementById('progress_bar').className='';", 2000);
+    }
+
+    // Read in the image file as a binary string.
+    reader.readAsBinaryString(evt.target.files[0]);
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+	
+}
             $(".clone_datepicker").datepicker({
 	beforeShowDay: function(date)
     {
@@ -162,8 +226,6 @@ function dialog_box(body_html_data,footer_html_data){
 	changeMonth: true,
 	changeYear: true,
 	buttonImage: baseUrl,       });	
-	
-}
 /******************---------------END--------------------------**********************/
  function number_format (number, decimals, dec_point, thousands_sep) {
     // *     example 1: number_format(1234.56);
@@ -234,5 +296,6 @@ function active_panel(div_object){
      	 $('.panel').removeClass('active-panel');
      	 $(div_object).parent().parent().parent().addClass('active-panel');
      }
+  
 
 
