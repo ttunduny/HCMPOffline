f2b7003ea1b45327c767ff1fed9ea650e3c473f2
Titus Tunduny <ttunduny@gmail.com>
2014-08-05 12:44:04 +0300

    Changes

--- application/controllers/rtk_management.php
+++ application/controllers/rtk_management.php
@@ -2607,6 +2607,61 @@ table.data-table td {border: none;border-left: 1px solid #DDD;border-right: 1px 
         echo "</table>";
     }
 
+    public function scmlt_home($msg=null,$popout=null){
+
+        if(isset($msq)){
+            $data['notif_message'] = $msg;
+        }
+        if(isset($popout)){
+            $data['popout'] = $popout;
+        }
+        $district = $this->session->userdata('district_id');        
+        $data['facilities'] = Facilities::get_total_facilities_rtk_in_district($district);
+        $facilities = Facilities::get_total_facilities_rtk_in_district($district);       
+        $district_name = districts::get_district_name_($district);                    
+        $table_body = '';
+        $reported = 0;
+        $nonreported = 0;
+
+        foreach ($facilities as $facility_detail) {
+
+           date_default_timezone_set("EUROPE/Moscow");
+                $lastmonth = date('F', strtotime("last day of previous month"));
+                $table_body .="<tr><td><a class='ajax_call_1' id='county_facility' name='" . base_url() . "rtk_management/get_rtk_facility_detail/$facility_detail[facility_code]' href='#'>" . $facility_detail["facility_code"] . "</td>";
+                $table_body .="<td>" . $facility_detail['facility_name'] . "</td><td>" . $district_name['district'] . "</td>";
+                $table_body .="<td>";
+
+                $lab_count = lab_commodity_orders::get_recent_lab_orders($facility_detail['facility_code']);
+//           echo "<pre>";print_r($lab_count);echo "</pre>";
+                if ($lab_count > 0) {
+                    $reported = $reported + 1;
+                    //".site_url('rtk_management/get_report/'.$facility_detail['facility_code'])."
+                    $table_body .="<span class='label label-success'>Submitted  for    $lastmonth </span><a href=" . site_url('rtk_management/rtk_orders') . " class='link'> View</a></td>";
+                } else {
+                    $nonreported = $nonreported + 1;
+                    $table_body .="<span class='label label-danger'>  Pending for $lastmonth </span> <a href=" . site_url('rtk_management/get_report/' . $facility_detail['facility_code']) . " class='link'> Report</a></td>";
+                }
+
+                $table_body .="</td>";
+            }
+            $county = $this->session->userdata('county_name');
+            $countyid = $this->session->userdata('county_id');
+            $data['countyid'] = $countyid;
+            $data['county'] = $county;
+            $data['table_body'] = $table_body;
+            $data['content_view'] = "rtk/rtk/dpp/dpp_home_with_table";
+            $data['title'] = "Home";
+            $data['link'] = "home";
+            $total = $reported + $nonreported;
+            $percentage_complete = $reported / $total * 100;
+            $percentage_complete = number_format($percentage_complete, 0);
+            $data['percentage_complete'] = $percentage_complete;
+            $data['reported'] = $reported;
+            $data['nonreported'] = $nonreported;
+            $this->load->view('rtk/template', $data);
+
+    }
+
     public function save_lab_report_data() {
 
         date_default_timezone_set("EUROPE/Moscow");
@@ -3403,7 +3458,10 @@ table.data-table td {border: none;border-left: 1px solid #DDD;border-right: 1px 
         $three_months_ago = date("Y-m-", strtotime("-1 Month"));
         $three_months_ago .='1';
 
-        $beg_date = date('Y-m-d', strtotime("first day of this Month"));
+        //$beg_date = date('Y-m-d', strtotime("first day of this Month"));
+        $beg_date = date('Y-m', strtotime("-1 Month"));
+        $end_date = date('Y-m-d', strtotime("last day of previous Month"));
+
 
 
         $sql = "SELECT facilities.facility_code,lab_commodity_details.id, lab_commodity_details.q_requested, lab_commodity_details.q_received,lab_commodity_details.commodity_id,lab_commodity_details.closing_stock,lab_commodity_details.beginning_bal,
@@ -3419,7 +3477,7 @@ table.data-table td {border: none;border-left: 1px solid #DDD;border-right: 1px 
         AND lab_commodity_orders.id = lab_commodity_details.order_id
         AND lab_commodity_details.commodity_id = lab_commodities.id
         AND lab_commodity_details.commodity_id BETWEEN 0 AND 6
-        AND lab_commodity_orders.order_date BETWEEN '$beg_date' AND NOW()
+        AND lab_commodity_orders.order_date BETWEEN '$beg_date' AND '$end_date'
         ORDER BY districts.district,facilities.facility_code  ASC,lab_commodity_details.commodity_id ASC ";
         $orders = $this->db->query($sql);
 //        echo "<pre>";print_r($orders->result_array());die;
