42407651f0217e0b671c815ceb047e6cdedcd5dd
Mureithi <kelvinmwas@gmail.com>
2014-08-20 09:48:56 +0300

    NEWs

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -505,7 +505,8 @@ class User extends MY_Controller {
 		$district_code = ($_POST['district_name']=='NULL')? 0: $_POST['district_name'];
 		$user_type = $_POST['user_type'];
 		$full_name= $fname .''.$lname; 
-		$county=$_POST['county'];
+		$county=$_POST['county_id'];
+		
 		switch ($identifier):
 			case 'moh':
 			
@@ -523,7 +524,7 @@ class User extends MY_Controller {
 			
 			break;
 			case 'super_admin':
-			
+				
 			case 'county':
 			$county=$this -> session -> userdata('county_id');
 			
@@ -532,7 +533,8 @@ class User extends MY_Controller {
 		     if($email_address!=''):
 		switch ($user_type):
 			case 10:
-			$savethis =  new Users();
+				$county=$_POST['county_id'];
+				$savethis =  new Users();
 				$savethis -> fname = $fname;
 				$savethis -> lname = $lname;
 				$savethis -> email = $email_address;
