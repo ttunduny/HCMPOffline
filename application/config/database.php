77280a9fc0314f96884b66b7a396a96368f34cbf
CollinsIchigo <collinsojenge@gmail.com>
2014-09-29 11:10:48 +0300

    Database Connection settings

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '#cmP@2014';
+$db['default']['password'] = '';
 $db['default']['database'] = 'hcmp_rtk';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
