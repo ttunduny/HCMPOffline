d445b504dc743339d19f8d72ed8820f59c4a0935
Titus Tunduny <ttunduny@gmail.com>
2014-12-09 18:19:49 +0300

    no message

--- update_labcommodities1_table.bat
+++
@@ -1,6 +0,0 @@
-cd c:\xampp\php
-php.exe -f c:\xampp\htdocs\HCMP\index.php rtk_management update_labs A
-php.exe -f c:\xampp\htdocs\HCMP\index.php rtk_management update_labs B
-php.exe -f c:\xampp\htdocs\HCMP\index.php rtk_management update_labs C
-php.exe -f c:\xampp\htdocs\HCMP\index.php rtk_management update_labs D
-
