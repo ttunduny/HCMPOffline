bed20356e5acc5e6f623f8040ca06987c85c7f95
Collins Ojenge <collinsojenge@gmail.com>
2015-01-21 11:28:53 +0300

    remade the sub county and county notifications side menu

--- application/views/subcounty/subcounty_home_v.php
+++ application/views/subcounty/subcounty_home_v.php
@@ -5,23 +5,13 @@
           </p>
         <div class="col-sm-3 col-md-2 sidebar-offcanvas"  id="bar" role="navigation" style="margin-left:0.5%">
            <div class="panel-group " id="accordion" style="padding: 0;">
-                <!--To be removed once the redesign is done
-                <div class="panel panel-default">
+           	<div class="panel panel-default">
                     <div class="panel-heading">
                         <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" id="notifications"><span class="glyphicon glyphicon-bullhorn">
-                            </span>Notifications</a>
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" id="consumption"><span class="glyphicon glyphicon-file">
+                            </span>Consumption</a>
                         </h4>
                     </div>
-                </div>-->
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" id="stocking_levels"><span class="glyphicon glyphicon-sort-by-attributes">
-                            </span>Stocking Levels</a>
-                        </h4>
-                    </div>
-                 
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -34,8 +24,8 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" id="consumption"><span class="glyphicon glyphicon-file">
-                            </span>Consumption</a>
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" id="program_reports"><span class="glyphicon glyphicon-folder-open">
+                            </span>Program Reports</a>
                         </h4>
                     </div>
                 </div>
@@ -47,24 +37,15 @@
                         </h4>
                     </div>
                 </div>
-                <!--<div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseSix" id="orders"><span class="glyphicon glyphicon-list-alt">
-                            </span>Orders</a>
-                        </h4>
-                    </div>
-                </div>-->
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" id="program_reports"><span class="glyphicon glyphicon-folder-open">
-                            </span>Program Reports</a>
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" id="stocking_levels"><span class="glyphicon glyphicon-sort-by-attributes">
+                            </span>Stocking Levels</a>
                         </h4>
                     </div>
                 </div>
-                <!-- Needs to be worked on ASAP Commented out till queries are redesigned. -->
-		<div class="panel panel-default">
+                <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4 class="panel-title">
                             <a data-toggle="collapse" data-parent="#accordion" href="#collapseEight" id="system_usage"><span class="glyphicon glyphicon-sort">
@@ -73,6 +54,26 @@
                     </div>
                     
                 </div>
+                <!--To be removed once the redesign is done
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" id="notifications"><span class="glyphicon glyphicon-bullhorn">
+                            </span>Notifications</a>
+                        </h4>
+                    </div>
+                </div>-->
+                <!--<div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseSix" id="orders"><span class="glyphicon glyphicon-list-alt">
+                            </span>Orders</a>
+                        </h4>
+                    </div>
+                </div>-->
+                
+                <!-- Needs to be worked on ASAP Commented out till queries are redesigned. -->
+				
             </div>
         </div>
       </div>
@@ -89,9 +90,9 @@
  */
   $(document).ready(function () {
   	//default 
-  $('.page-header').html('Stocking Levels');
-  $('#stocking_levels').parent().parent().parent().addClass('active-panel');
-  ajax_request_replace_div_content('reports/stock_level_dashboard',"#notification");
+  $('.page-header').html('Consumption');
+  $('#consumption').parent().parent().parent().addClass('active-panel');
+  ajax_request_replace_div_content('reports/consumption_data_dashboard/NULL/NULL/NULL/NULL/NULL/NULL/NULL/1',"#notification");
  
  
   $('[data-toggle=offcanvas]').click(function () {
