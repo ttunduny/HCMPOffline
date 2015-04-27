2bfdb2e7b5eeccba5fdfd6ba115dd175316310b5
Kelvin <kelvinmwas@gmail.com>
2015-04-27 11:13:43 +0300

    before merge-Seth

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -3850,7 +3850,13 @@ class Reports extends MY_Controller {
 			$total_sent = round(($sent_units / $total_units), 1);
 			$total_received = round(($received_units / $total_units), 1);
 			///date_sent
-			$date_received = strtotime($facility_expiry_data['date_received']) ? date('d M, Y', strtotime($facility_expiry_data['date_received'])) : "N/A";
+			if ($facility_expiry_data['date_received']=='') {
+				$date_received='N/A';
+			} else {
+			$date_received=	date('d M, Y', strtotime($facility_expiry_data['date_received']));
+			}
+			
+			//$date_received = strtotime($facility_expiry_data['date_received']) ? date('d M, Y', strtotime($facility_expiry_data['date_received'])) : "N/A";
 			$status = $facility_expiry_data['status'] == 0 ? "<span class='label label-danger'>Pending</span>" : ($facility_expiry_data['status'] == 1 ? "<span class='label label-success'>Received</span>" : null);
 			array_push($series_data, array($facility_expiry_data['source_facility_name'] . " :" . $facility_expiry_data['source_facility_code'], $facility_expiry_data['receiver_facility_name'] . " :" . $facility_expiry_data['receiver_facility_code'], $facility_expiry_data['commodity_name'], $facility_expiry_data['source_district'], $facility_expiry_data['receiver_district'], $facility_expiry_data['unit_size'], $facility_expiry_data['batch_no'], date('d M, Y', strtotime($facility_expiry_data['expiry_date'])), $facility_expiry_data['manufacturer'], $total_sent, $sent_units, $total_received, $received_units, date('d M, Y', strtotime($facility_expiry_data['date_sent'])), $date_received, $status));
 		endforeach;
