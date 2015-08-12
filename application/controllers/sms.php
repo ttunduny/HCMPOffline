633380058d4902159bfd94716b82a2cf20b922a7
Richard Karsan <karsanrichard@gmail.com>
2015-08-12 23:04:15 +0300

    Changed variable name for active facilities

--- application/controllers/sms.php
+++ application/controllers/sms.php
@@ -2587,9 +2587,9 @@ public function log_summary_weekly(){
 		$decommissioned = Facilities::get_facility_data_specific('last_decommissioned',$county_id,$district_id,$facility_code,'all');
 		$redistributed = Facilities::get_facility_data_specific('last_redistributed',$county_id,$district_id,$facility_code,'all');
 		$added_stock = Facilities::get_facility_data_specific('last_added_stock',$county_id,$district_id,$facility_code,'all');
-		$all_faciliteis = Facilities::getAll_();
+		$active_facilities = Facilities::getAll_();
 
-		// echo "<pre>";print_r($all_faciliteis);echo "</pre>";exit;
+		// echo "<pre>";print_r($active_facilities);echo "</pre>";exit;
 		$final_array = array();
 		$last_seen_count = count($last_seen);
 		$last_issued_count = count($last_issued);
