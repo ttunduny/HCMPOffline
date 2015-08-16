433cf1b3849168ede53cff388e0b91854f145de2
HCMPKenya <hcmp.kenya@gmail.com>
2015-08-16 23:32:06 +0300

    Hcmp_functions.php updated

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-define('ENVIRONMENT', 'testing');
+define('ENVIRONMENT', 'production');
 /*
  * ---------------------------------------------------------------
  * ERROR REPORTING
@@ -52,7 +52,7 @@ if (defined('ENVIRONMENT'))
 	
 		case 'testing':
 		case 'production':
-			error_reporting(1);
+			error_reporting(0);
 		break;
 
 		default:
