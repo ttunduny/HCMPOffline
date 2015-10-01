d549988382870f01d127d89bf6139c03f4b498e6
Richard Karsan <karsanrichard@gmail.com>
2015-10-01 11:56:18 +0300

    no message

--- application/views/shared_files/login_pages/login_v.php
+++ application/views/shared_files/login_pages/login_v.php
@@ -136,7 +136,6 @@
 
 			<div class="row">
 			<div class="col-md-3">
-					<a href="http://41.89.6.209/hcmp_demo/national/demo_accounts"><button class="btn btn-primary ">Demo Site Login Credentials </button></a>
 				 <span>
 				 	<h5>Experiencing any challenges?<br />
 						Send an email to: hcmphelpdesk@googlegroups.com</h5> 
