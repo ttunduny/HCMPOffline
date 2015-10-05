51a1f8522c19f2499a20df895e79484e60414a2b
Titus Tunduny <ttunduny@gmail.com>
2015-10-05 16:54:50 +0300

    Changes to rectify the batch issue on the district store

--- application/views/subcounty/drug_store/drug_store_recieve_items_v.php
+++ application/views/subcounty/drug_store/drug_store_recieve_items_v.php
@@ -140,6 +140,7 @@ var facility_stock_data=<?php echo $facility_stock_data;     ?>;
 	     	    	
 			var commodity_id=data_array[0];
 			var stock_data=extract_data(data_array[0],commodity_id,'batch_data');
+			console.log(stock_data);
             var dropdown="<option special_data=''>--select Batch--</option>"+stock_data[0];
             var facility_stock_id=stock_data[1];
             var total_stock_bal=data_array[4];
@@ -168,10 +169,11 @@ var facility_stock_data=<?php echo $facility_stock_data;     ?>;
 				locator.closest("tr").find(".batch_no").html(dropdown);
 				locator.closest("tr").find(".expiry_date").val(""+stock_data[3]+"" );
 				locator.closest("tr").find(".balance").val(remaining_items);
-				locator.closest("tr").find(".available_stock").val(stock_data[2]-total_issues_for_this_batch);		
+				locator.closest("tr").find(".available_stock").val(remaining_items);		
+				// locator.closest("tr").find(".available_stock").val(stock_data[2]-total_issues_for_this_batch);		
 				locator.closest("tr").find(".commodity_id").val(commodity_id);
 				locator.closest("tr").find(".commodity_balance").val(remaining_items);	
-		});//entering the values to issue check if you have enough balance
+		});///entering the values to issue check if you have enough balance
            $(".quantity_issued").on('keyup',function (){
         	var bal=parseInt($(this).closest("tr").find(".available_stock").val());
         	var bal1=parseInt($(this).closest("tr").find(".commodity_balance").val());
