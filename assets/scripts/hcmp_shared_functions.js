b396185f8fe05c33269f8b99b5197313cc5e91a2
Titus Tunduny <ttunduny@gmail.com>
2015-10-07 18:23:16 +0300

    Quick Fix to Save the Add Edit Form before saving notification

--- assets/scripts/hcmp_shared_functions.js
+++ assets/scripts/hcmp_shared_functions.js
@@ -274,11 +274,11 @@ function confirm_with_summary(html_data,form_id){
 				     closeOnCancel: false }, 
 				     
 				     function(isConfirm){   if (isConfirm) {   
-				     	$(form_id).submit();  
-				     	 swal("Saved!", "Your data was saved.", "success"); 
+				     	$(form_id).submit();  				     	
 				     	 if($(form_id).valid()){
 					           $(form_id).submit();      
 					          }
+                    swal("Saved!", "Your data was saved. ", "success"); 
 				     	 
 				     	   } else {
 				     	   	
