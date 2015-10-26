af3148802ddf42791a814704dffa3a7ae44b5350
Titus Tunduny <ttunduny@gmail.com>
2015-10-26 11:11:49 +0300

    County and subcounty expiries fix on the duplicate side menu

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -4183,8 +4183,8 @@ class Reports extends MY_Controller {
 		$data['report_view'] = "subcounty/reports/county_expiries_v";
 		$data['sidebar'] = "shared_files/report_templates/side_bar_sub_county_v";
 		$data['active_panel'] = 'expiries';
-		// return $this -> load -> view("subcounty/reports/county_expiries_v", $data);
-		return $this -> load -> view("shared_files/template/template", $data);
+		return $this -> load -> view("subcounty/reports/county_expiries_v", $data);
+		// return $this -> load -> view("shared_files/template/template", $data);
 
 	}
 	public function county_consumption() {
