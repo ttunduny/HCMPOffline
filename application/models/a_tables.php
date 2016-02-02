53dae5377607fe4490d94a352c0acfb18d9885d8
George-Kagwe <georgegkagwe@gmail.com>
2016-02-02 14:40:24 +0300

    Data synchronization for offline

---
+++ application/models/a_tables.php
@@ -0,0 +1,26 @@
++-------------------------------------+
+|       |
+| facility_amc1                       |
+| facility_evaluation                 |
+| facility_impact_evaluation          |
+| facility_issues                     |
+| facility_monthly_stock              |
+| facility_order_details              |
+| facility_order_details_rejects      |
+| facility_order_status               |
+| facility_orders                     |
+| facility_stock_out_tracker          |
+| facility_stock_status               |
+| facility_stocks                     |
+| facility_stocks_temp                |
+| facility_transaction_table          |
+| issue_type                          |
+| log                                 |
+| log_monitor                         |
+| malaria_data                        |
+| malaria_drugs                       |
+| menu                                |
+| messages                            |
+| patient_details                     |
+| patient_issues                      |
+|
