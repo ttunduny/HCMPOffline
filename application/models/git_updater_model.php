75cd025a342746eb47cad60ea56f90bdf04defc1
karsanrichard <karsanrichard@gmail.com>
2016-02-24 22:41:22 +0300

    modified:   application/models/git_updater_model.php

--- application/models/git_updater_model.php
+++ application/models/git_updater_model.php
@@ -1,14 +1,6 @@
 <?php
 class Git_updater_model extends Doctrine_Record {
-<<<<<<< HEAD
-	public  function get_latest_hash(){
-		$latest_commit = $this->db->query("SELECT * FROM hcmp_rtk.git_log WHERE update_time = (SELECT MAX(update_time) from git_log)");
 
-		return $latest_commit->result_array();
-	}
-}
-?>
-=======
 	public function get_latest_hash(){
 		$latest_hash = $this->db->query('SELECT * FROM git_log WHERE date_added = (SELECT MAX(date_added) FROM git_log)');
 
@@ -18,4 +10,3 @@ class Git_updater_model extends Doctrine_Record {
 	
 
 }
->>>>>>> cfaed47d0f5f3941855efdacade5afb4237ab353
