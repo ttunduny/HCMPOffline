7edbc42947c001cc539ef6cc6df3cd6cc6914bd6
Erick Lesan <lesaneric@gmail.com>
2016-04-11 10:33:44 +0300

    no message

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,5 +5,7 @@ class  MY_Controller  extends  CI_Controller  {
     function __construct()
     {
         parent::__construct(); 
+    	$this->output->enable_profiler(TRUE);
+        
     }
     } 
