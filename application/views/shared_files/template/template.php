d162a86f499e0d04ceb940a076934d50e2f1c9f5
Richard Karsan <karsanrichard@gmail.com>
2016-04-19 22:02:40 +0300

    Online status badge at top of screen

--- application/views/shared_files/template/template.php
+++ application/views/shared_files/template/template.php
@@ -301,8 +301,8 @@ if (!$this -> session -> userdata('user_id')) {
   </div>
   <center>
     <div class="col-md-4"style="font-weight:bold;">  
-    <!-- <div id="online-top" class="online-status"><span class="green"><span style="font-weight: bold" class="glyphicon glyphicon-off small-margin"></span>On</span>line</div> -->
-    <div id="offline-top" class="online-status"><span class="red"><span style="font-weight: bold" class="glyphicon glyphicon-off small-margin"></span>Off</span>line</div>
+    <div id="online-top" class="online-status"><span class="green"><span style="font-weight: bold" class="glyphicon glyphicon-off small-margin"></span>On</span>line</div>
+    <!-- <div id="offline-top" class="online-status"><span class="red"><span style="font-weight: bold" class="glyphicon glyphicon-off small-margin"></span>Off</span>line</div> -->
       <span><?php echo $this-> session -> userdata('facility_count'); ?>  </span>			
     </div>
   </center>
